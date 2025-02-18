6ec0df40ff834447a09abe9204333338436f00a2
jonz94 <jody16888@gmail.com>
2025-02-18 21:18:18 +0800

    fix: improve bopomofoify

--- lib/bopomofoify.ts
+++ lib/bopomofoify.ts
@@ -2,19 +2,24 @@ import initializeJiebaWASM, { add_word, cut } from 'jieba-wasm'
 
 const lookupTable: Record<string, string> = {
   是不是: '484',
+  還是: '還4',
   是: '4',
   我: '窩',
   你: 'ㄋ',
   妳: 'ㄋ',
   了: 'ㄌ',
+  真的: '真ㄉ',
   的: 'ㄉ',
+  某個: '某ㄍ',
+  有個: '有ㄍ',
   個: 'ㄍ',
   他: 'ㄊ',
   她: 'ㄊ',
   牠: 'ㄊ',
   它: 'ㄊ',
-  好: '毫',
+  好: '豪',
   超: '敲',
+  超可愛: '敲口i',
   可愛: '口i',
   做愛: '捉i',
   大便: '答辯',
@@ -22,6 +27,7 @@ const lookupTable: Record<string, string> = {
   喔: 'ㄛ',
   嗎: 'ㄇ',
   欸: 'ㄟ',
+  吧: 'ㄅ',
 }
 
 let alreadyInitialized = false
@@ -33,6 +39,10 @@ export async function initialize() {
 
   await initializeJiebaWASM()
 
+  add_word('超可愛')
+  add_word('可愛')
+  add_word('好在')
+  add_word('做愛')
   add_word('翻湧')
 
   alreadyInitialized = true
@@ -42,6 +52,8 @@ export async function bopomofoify(input: string) {
   await initialize()
 
   const words = cut(input, true)
+
+  // console.log(JSON.stringify(words, null, 2))
 
   return words.map((word) => lookupTable[word] ?? word).join('')
 }
