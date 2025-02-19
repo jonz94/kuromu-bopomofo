1c43ccd2c69f4375f795e39170768d6dbea2e0bf
jonz94 <jody16888@gmail.com>
2025-02-19 20:36:20 +0800

    feat: add subscribe button

--- app/bopomofoify.tsx
+++ app/bopomofoify.tsx
@@ -96,7 +96,24 @@ export function Bopomofoify() {
         </div>
       </div>
 
-      <div className="flex justify-end">
+      <div className="flex justify-between">
+        <Button className="[--primary:0_100_50%]" asChild>
+          <a
+            href="https://www.youtube.com/channel/UC2ZWggon1NOT2TGaVUMzY7A"
+            className="flex items-center justify-center"
+          >
+            <span>
+              <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"
+                  fill="currentColor"
+                />
+              </svg>
+            </span>
+            <span className="relative -top-px">訂閱庫洛姆</span>
+          </a>
+        </Button>
+
         <Button
           variant="secondary"
           onClick={async () => {
